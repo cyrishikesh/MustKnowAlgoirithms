2dee3b8dc2bfe66c5bb6df1792c44fa46fcd9c14
Rishikesh Chaudhary <cyrishikesh@gmail.com>
2020-06-29 21:30:23 +0530

    added lps array computation function

---
+++ KMP.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// this function will return all the index in the string text at which string pattern is found
+// in the form of vector
+
+vector<int> foundPatternIndex(vector<int> lps,string text,string pattern){
+
+}
+
+// this function will return the longest proper prefix vector for string pattern
+
+vector<int> createLongestProperPrefix(string pattern){
+    int n=pattern.size();
+    vector<int> lps(n,0);
+    for(int i=1;i<n;i++)
+    {
+        int j=lps[i-1];
+        while(j>0 && pattern[j]!=pattern[i]) j=lps[j-1];
+        if(pattern[i]==pattern[j]) j++;
+        lps[i]=j;
+    }
+    return lps;
+}
+
+// this function will print all the indexex at the which pattern is found in the string text
+void printFoundPatternIndex(vector<int> fpi){
+     int n=fpi.size();
+     for(int i=0;i<n;i++){
+         cout<<"string found at position "<<fpi[i]<<" "<<" in the string text."<<endl;
+     }
+     return ;
+}
+int main(){
+    string text,pattern;
+    cin>>text>>pattern;
+    // lps is longest proper prefix vector
+    vector<int> lps=createLongestProperPrefix(pattern);
+
+    // fpi is found pattern indexes
+    vector<int> fpi=foundPatternIndex(lps,text,pattern);
+
+    
+    return 0;
+}
